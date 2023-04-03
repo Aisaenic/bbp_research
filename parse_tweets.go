4bea1a8ebca5e50f1beecd25e100fe0a671b3f4b
Hava-Kantrowitz <46357722+Hava-Kantrowitz@users.noreply.github.com>
2023-04-03 14:10:35 -0400

    CHANGING TO PY DONT USE
    Processing file in go, realized go is really, really not the right language to use for this. Changing to python. Enough Go has been learned for ACE.

--- parse_tweets.go
+++ parse_tweets.go
@@ -2,106 +2,143 @@ package main
 
 import (
 	"fmt"
-//	"encoding/json"
+	"encoding/json"
 	"os"
 	"io/ioutil"
 )
 
 type Tester struct {
-        FirstName string
-        LastName string
+	FirstName string `json:"FirstName"`
+	LastName string `json:"LastName"`
 }
 
 type Media struct {
-        previewUrl string
-        fullUrl string
-        altText string
+	PreviewUrl string `json:"previewUrl"`
+	FullUrl string `json:"fullUrl"`
+	AltText string `json:"altText"`
 }
 
 type Link struct {
-        text string
-        url string
-        tcourl string
-        indices string
+	Text string `json:"text"`
+	Url string `json:"url"`
+	Tcourl string `json:"tcourl"`
+	Indices string `json:"indices"`
 }
 
 type User struct {
-        username string
-	id string
-	displayName string
-	rawDescription string 
-	renderedDescription string
-	descriptionLinks string
-	verified string
-	created string
-	followersCount string
-	friendsCount string
-	statusesCount string
-	favoritesCount string
-	listedCount string
-	mediaCount string
-	location string
-	protected string 
-	link Link
-	profileImageUrl string
-	profileBannerUrl string
-	label string
-	description string
-	descriptionUrls string
-	linkUrl string
-	url string
+	Username string `json:"username"`
+	Id string `json:"id"`
+	DisplayName string `json:"displayname"`
+	RawDescription string `json:"rawDescription"`
+	RenderedDescription string `json:"renderedDescription"`
+	DescriptionLinks string `json:"descriptionLinks"`
+	Verified string `json:"verified"`
+	Created string `json:"created"`
+	FollowersCount string `json:"followersCount"`
+	FriendsCount string `json:"friendsCount"`
+	StatusesCount string `json:"statusesCount"`
+	FavoritesCount string `json:"favoritesCount"`
+	ListedCount string `json:"listedCount"`
+	MediaCount string `json:mediaCount"`
+	Location string `json:"location"`
+	Protected string `json:"protected"`
+	Link Link `json:"link"`
+	ProfileImageUrl string `json:"profileImageUrl"`
+	ProfileBannerUrl string `json:"profileBannerUrl"`
+	Label string `json:"label"`
+	Description string `json:"description"`
+	DescriptionUrls string `json:"descriptionUrls"`
+	LinkTcourl string `json:"linkTcourl"`
+	LinkUrl string `json:"linkUrl"`
+	Url string `json:"url"`
 }
 
 type Tweet struct {
-	url string
-	date string
-	rawContent string
-	renderedContent string
-	id string 
-	user User
-	replyCount string
-	retweetCount string
-	likeCount string
-	quoteCount string
-	conversationId string
-	lang string 
-	source string
-	sourceUrl string
-	sourceLabel string 
-	link Link
-	media Media
-	retweetedTweet string
-	quotedTweet string
-	inReplyToTweetId string
-	inReplyToUser string
-	mentionedUsers string 
-	coordinates string 
-	place string
-	hashtags string
-	cashtags string 
-	card string 
-	viewCount string 
-	vibe string 
-	content string 
-	outlinks string
-	outlinksss string
-	tcoutlinks string
-	tcoutlinksss string 
-	username string
-	classification bool 
+	Url string `json:"url"`
+	Date string `json:"date"`
+	RawContent string `json:"rawContent"`
+	RenderedContent string `json:"renderedContent"`
+	Id string `json:"id"`
+	User User `json:"user"`
+	ReplyCount string `json:"replyCount"`
+	RetweetCount string `json:"retweetCount"`
+	LikeCount string `json:"likeCount"`
+	QuoteCount string `json:"quoteCount"`
+	ConversationId string `json:"conversationId"`
+	Lang string `json:"lang"`
+	Source string `json:"source"`
+	SourceUrl string `json:"sourceUrl"`
+	SourceLabel string `json:"sourceLabel"`
+	Links Link `json:"links"`
+	Media Media `json:"media"`
+	RetweetedTweet string `json:"retweetedTweet"`
+	QuotedTweet string `json:"quotedTweet"`
+	InReplyToTweetId string `json:"inReplyToTweetId"`
+	InReplyToUser string `json:"inReplyToUser"`
+	MentionedUsers string `json:"mentionedUsers"`
+	Coordinates string `json:"coordinates"`
+	Place string `json:"place"`
+	Hashtags string `json:"hashtags"`
+	Cashtags string `json:"cashtags"`
+	Card string `json:"card"`
+	ViewCount string `json:"viewCount"`
+	Vibe string `json:"vibe"`
+	Content string `json:"content"`
+	Outlinks string `json:"outlinks"`
+	Outlinksss string `json:"outlinksss"`
+	Tcoutlinks string `json:"tcoutlinks"`
+	Tcoutlinksss string `json:"tcoutlinksss"`
+	Username string `json:"username"`
+	Classification bool 
+}
+
+//Tweets struct which contains an array of Tweets
+type Tweets struct {
+	Tweets []Tweet 
+}
+
+type Testers struct {
+	Testers []Tester
 }
 
 func parse_input(in_file string) {
-	data, err := ioutil.ReadFile(in_file)
+	jsonFile, err := os.Open(in_file)
 	if err != nil {
-		fmt.Println("Cannot read file")
+		fmt.Println("Cannot read file", err)
 		os.Exit(1)
 	}
 
-	test_data_map := make(map[int]Tester)
+	defer jsonFile.Close()
 
-	fmt.Println("File content is:")
-	fmt.Println(string(data))
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	var tweets Tweets 
+	json.Unmarshal(byteValue, &tweets)
+	fmt.Println(tweets)
+} 
+
+func test_parse_input(in_file string) {
+	jsonFile, err := os.Open(in_file)
+	if err != nil {
+		fmt.Println("Cannot read file", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Successfully opened", in_file)
+
+	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	var testers Testers 
+
+	json.Unmarshal(byteValue, &testers)
+
+	fmt.Println(testers) 
+
+//	for i := 0; i < len(tweets.Tweets); i++ {
+//		fmt.Println("content: " + tweets.Tweets[i].content)
+//	}
+
 }
 
 func main() {
