df587a0311c5e1f55bc12d70d392bf09cbe344e4
Hava-Kantrowitz <46357722+Hava-Kantrowitz@users.noreply.github.com>
2023-04-05 15:41:10 -0400

    finished parsing, full tagged data

--- parse_tweets.py
+++ parse_tweets.py
@@ -2,13 +2,16 @@
 
 import argparse
 import json 
+import csv
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("file")
+    parser.add_argument("all_data")
+    parser.add_argument("tagged_data")
     args = parser.parse_args()
-    file = args.file
-    return file
+    all_data = args.all_data
+    tagged_data = args.tagged_data
+    return [all_data, tagged_data]
 
 def ingest_file(file):
     tweets = []
@@ -33,10 +36,66 @@ def key_parts(tweets):
         content = val["content"]
         print(content.replace("\n", " "))
 
+def ingest_tags(file):
+    tags = []
+    openf = open(file)
+    for line in openf:
+        line = line.replace("\n", "")
+        line = line.split(",")
+        id_val = line[0]
+        tag = line[1]
+        tag_dict = {"id": id_val, "tag": tag}
+        tags.append(tag_dict)
+    return tags
+
+def connect_tags(all_data, tagged_data):
+    good_data = []
+    for val in all_data:
+        del val["_type"]
+        del val["user"]["_type"]
+        del val["user"]["link"]
+        del val["links"]
+        del val["outlinks"]
+        del val["tcooutlinks"] 
+
+        for elt in val["user"]:
+            fieldName = "user_" + elt
+            val[fieldName] = val["user"][elt]
+        del val["user"] 
+        
+        if type(val["media"]) is list:
+            val["altText"] = val["media"][0]["altText"]
+            del val["media"]
+
+        curr_id = val["id"]
+        
+        for num in tagged_data:
+            temp_id = int(num["id"])
+            if curr_id == temp_id:
+                val["tag"] = num["tag"] 
+        
+        good_data.append(val)
+    return good_data
+
+def write_to_csv(data):
+    with open('full_dataset.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        field_names = data[0].keys()
+        writer.writerow(field_names) 
+
+        for line in data:
+            vals = []
+            for elt in line:
+                vals.append(line[elt])
+            print(vals)
+            writer.writerow(vals) 
+
 def main():
-    file = parse_args()
-    all_tweets = ingest_file(file)
+    files = parse_args()
+    all_tweets = ingest_file(files[0])
+    tags = ingest_tags(files[1])
     tweets = unique_tweets(all_tweets)
-    key_parts(tweets) 
+    parsed_data = connect_tags(tweets, tags)
+    write_to_csv(parsed_data) 
 
 main()
