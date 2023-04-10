4cdc2559a6671418fd37d7ad1709e45b0421ce05
Hava-Kantrowitz <46357722+Hava-Kantrowitz@users.noreply.github.com>
2023-04-10 17:58:57 -0400

    add comments

--- parse_tweets.py
+++ parse_tweets.py
@@ -4,6 +4,9 @@ import argparse
 import json 
 import csv
 
+#Parses command line args, two command line args exist, json file with all data and csv file with data tags
+#Input: None 
+#Output: list with json file all_data and csv file tagged_data
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("all_data")
@@ -13,15 +16,20 @@ def parse_args():
     tagged_data = args.tagged_data
     return [all_data, tagged_data]
 
+#Parses all tweet data 
+#Input: json file
+#Output: List of tweets, where each tweet is represented as a multidimensional dict
 def ingest_file(file):
     tweets = []
     openf = open(file)
     for line in openf: 
         data = json.loads(line)
         tweets.append(data)
-
     return tweets 
 
+#Removes repeat tweets 
+#Input: list of tweets 
+#Output: list of tweets with any duplicate tweets removed
 def unique_tweets(tweets): 
     id_list = []
     unique_tweets = []
@@ -31,11 +39,18 @@ def unique_tweets(tweets):
             unique_tweets.append(val)
     return unique_tweets
 
+#Isolates specific elements of a tweet
+#Input: list of tweets 
+#Output: N/A 
 def key_parts(tweets): 
+    #currently isolates tweet content, that can be changed 
     for val in tweets: 
         content = val["content"]
         print(content.replace("\n", " "))
 
+#Processes the data tags 
+#Input: csv file 
+#Output: list of tags, where each element in list is dictionary with id/tag pairs 
 def ingest_tags(file):
     tags = []
     openf = open(file)
@@ -48,9 +63,14 @@ def ingest_tags(file):
         tags.append(tag_dict)
     return tags
 
+#Cleans and prepares data for classification
+#Adds tag to its corresponding data, removes extraneous fields, and replaces NaN values with 0
+#Input: list of tweets, list of tags 
+#Output: list where each element is a dictionary representing fully cleaned tweets 
 def connect_tags(all_data, tagged_data):
     good_data = []
     for val in all_data:
+        #delete extraneous fields
         del val["_type"]
         del val["user"]["_type"]
         del val["user"]["link"]
@@ -58,17 +78,20 @@ def connect_tags(all_data, tagged_data):
         del val["outlinks"]
         del val["tcooutlinks"] 
 
+        #flatten three dimensional user field 
         for elt in val["user"]:
             fieldName = "user_" + elt
             val[fieldName] = val["user"][elt]
         del val["user"] 
         
+        #flatten three dimensional media field 
         if type(val["media"]) is list:
             val["altText"] = val["media"][0]["altText"]
             del val["media"]
 
         curr_id = val["id"]
         
+        #add the proper tag to its corresponding tweet 
         for num in tagged_data:
             temp_id = int(num["id"])
             if curr_id == temp_id:
@@ -77,12 +100,16 @@ def connect_tags(all_data, tagged_data):
                 elif num["tag"] == "TRUE":
                     val["tag"] = 1
        
+        #change NaN data to ints with 0 
         for elt in val: 
             if type(val[elt]) is not str and type(val[elt]) is not int and type(val[elt]) is not bool:
                 val[elt] = 0 
         good_data.append(val)
     return good_data
 
+#Write cleaned data to csv 
+#Input: list of cleaned tweets 
+#Output: N/A 
 def write_to_csv(data):
     with open('full_dataset.csv', 'w', newline='') as file:
         writer = csv.writer(file)
